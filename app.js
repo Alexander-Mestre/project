142726c5f905857dbeb9dde4e85c55061a7f6736
Alexander-Mestre <59705436+Alexander-Mestre@users.noreply.github.com>
2021-05-22 15:22:16 -0600

    fixed heroku

--- app.js
+++ app.js
@@ -64,10 +64,14 @@ app.use(authRoutes);
 
 app.use(errorController.get404);
 
+const PORT = process.env.PORT || 3000;
+
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-    app.listen(3000);
+    app.listen(PORT, () => {
+      console.log(`Our app is running on port ${ PORT }`);
+  });
   })
   .catch(err => {
     console.log(err);
