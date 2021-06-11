22d25d89bc796c6b655c0f02733f35391d85fff5
Alexander-Mestre <59705436+Alexander-Mestre@users.noreply.github.com>
2021-06-10 19:27:39 -0600

    templating the app

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const MONGODB_URI =
-  'mongodb+srv://alexandermestre:Barcelona123@instance1project1.ytlxc.mongodb.net/project?retryWrites=true&w=majority';
+  'mongodb+srv://alexandermestre:Barcelona123@instance1project1.ytlxc.mongodb.net/magazine?retryWrites=true&w=majority';
 
 const app = express();
 const store = new MongoDBStore({
